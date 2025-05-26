1acda628d11ed70615a412668315d751d7217463
Martin-IB <martin.chipana@utec.edu.pe>
2025-05-26 17:58:32 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -155,7 +155,8 @@ def page_not_found(error):
     return render_template('error.html')
 
 if __name__ == '__main__':
-    app.run(port=3000, host="0.0.0.0",debug=True)
+    
+    app.run(host='0.0.0.0', port=5000)
 
 
 
