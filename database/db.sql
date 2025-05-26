1705eff186d65d5baa81ad25283a71faf8a07b41
Martin <mart15chhu@gmail.com>
2025-05-26 16:08:45 -0500

    first commit

--- database/db.sql
+++ database/db.sql
@@ -1,40 +1,29 @@
-drop database db_poo;
+DROP DATABASE IF EXISTS db_poo;
+CREATE DATABASE db_poo;
+USE db_poo;
 
-create database db_poo;
-
-use db_poo;
-
+-- Tabla usuarios
 CREATE TABLE usuario (
-  id int(5) NOT NULL primary key auto_increment,
-  nombre varchar(50) NOT NULL,
-  telefono varchar(50) NOT NULL,
-  email varchar(50) NOT NULL
+  id INT(5) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  nombre VARCHAR(50) NOT NULL,
+  telefono VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=latin1;
 
-insert into usuario(nombre,telefono,email)
-values ('Jaime','999888777','jaime@abc.com');
+INSERT INTO usuario(nombre, telefono, email)
+VALUES ('Jaime', '999888777', 'jaime@abc.com');
 
-
-
-
-"""
-Base de datos para empleados
-"""
-
-create database db_poo;
-
-use db_poo;
-
+-- Tabla empleados
 CREATE TABLE empleados (
-  id int(5) NOT NULL primary key auto_increment,
-  nombre varchar(50) NOT NULL,
-  apellido varchar(50) NOT NULL,
-  email varchar(50) NOT NULL UNIQUE,
-  DNI int(8) NOT NULL UNIQUE
+  id INT(5) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  nombre VARCHAR(50) NOT NULL,
+  apellido VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL UNIQUE,
+  DNI INT(8) NOT NULL UNIQUE
 ) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=latin1;
 
-insert into empleados(nombre,apellido,email,DNI)
-values ('Daniel','Montes','dan@abc.com',68382828);
+INSERT INTO empleados(nombre, apellido, email, DNI)
+VALUES ('Daniel', 'Montes', 'dan@abc.com', 68382828);
 
 
 
